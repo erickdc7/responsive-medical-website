d89442d8041db80dfac082980287f99cb4993ec2
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-12-09 12:34:48 -0500

    add img animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,3 +130,4 @@ sr.reveal(`.home__button`, { origin: 'left', delay: 1800 })
 
 sr.reveal(`.delivery__data`, { origin: 'right' })
 sr.reveal(`.delivery__content`, { origin: 'left', delay: 600 })
+sr.reveal(`.delivery__img`, { delay: 1200 })
